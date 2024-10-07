b8f0e4d0a31b508c5396e76d10fbedb83b8fa56d
Baptiste D <baltazar.14130@gmail.com>
2024-10-07 08:35:39 +0200

    login page clicked

---
+++ src/app/login/login-page/login-page.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatFormField} from "@angular/material/form-field";
+import {MatButton} from "@angular/material/button";
+import {MatInput} from "@angular/material/input";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [
+    MatCardContent,
+    MatFormField,
+    MatCardTitle,
+    MatCardHeader,
+    MatCard,
+    MatButton,
+    MatInput,
+    ReactiveFormsModule
+  ],
+  templateUrl: './login-page.component.html',
+})
+export class LoginPageComponent {
+
+  loginForm: FormGroup=new FormGroup({
+    email: new FormControl('',Validators.required),
+    password: new FormControl('',Validators.required)
+  })
+
+
+  onSubmit(){
+    console.log("submit clicked")
+  }
+
+}
