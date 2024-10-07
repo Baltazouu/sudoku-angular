26dca51de7e95e550cbb7d10a371668c9ce3bb9b
clement.verdoire <clem.v08@gmail.com>
2024-10-07 09:09:29 +0200

    dev login page

--- src/app/login/login-page/login-page.component.ts
+++ src/app/login/login-page/login-page.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
-import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
-import {MatError, MatFormField} from "@angular/material/form-field";
-import {MatButton} from "@angular/material/button";
-import {MatInput} from "@angular/material/input";
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from "@angular/material/card";
+import { MatError, MatFormField } from "@angular/material/form-field";
+import { MatButton } from "@angular/material/button";
+import { MatInput } from "@angular/material/input";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login-page',
@@ -17,20 +20,33 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/
     MatButton,
     MatInput,
     ReactiveFormsModule,
-    MatError
+    MatError,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
   ],
   templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
 
-  loginForm: FormGroup=new FormGroup({
-    email: new FormControl('',Validators.required),
-    password: new FormControl('',Validators.required)
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
   })
 
-
-  onSubmit(){
-    console.log("submit clicked")
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
   }
 
+
+  onSubmit() {
+    console.log("submit clicked")
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+  }
 }
