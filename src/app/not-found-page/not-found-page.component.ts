09d4e944e7e33967c1d3a6ebf19198dfd0cda2cc
clement.verdoire <clem.v08@gmail.com>
2024-10-08 22:59:08 +0200

    ✨ improve 404 page

--- src/app/not-found-page/not-found-page.component.ts
+++ src/app/not-found-page/not-found-page.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
-import {MatAnchor} from "@angular/material/button";
+import { RouterLink } from "@angular/router";
+import { MatAnchor } from "@angular/material/button";
+
+//Assets
+
+//Components
+import { NavbarComponent } from "../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-not-found-page',
   standalone: true,
   imports: [
     RouterLink,
-    MatAnchor
+    MatAnchor,
+    NavbarComponent
   ],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.scss'
