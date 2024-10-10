277dbabeb6491e71f7b0b15c4cea5970806f7fee
clement.verdoire <clem.v08@gmail.com>
2024-10-10 14:45:23 +0200

    💄 🐛  routing and sudoku display

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,17 @@
 import { Routes } from '@angular/router';
-import {LoginPageComponent} from "./login-page/login-page.component";
-import {HomePageComponent} from "./home-page/home-page.component";
-import {AuthGuard} from "./auth/auth-guard";
-import {NotFoundPageComponent} from "./not-found-page/not-found-page.component";
-import {RankingPageComponent} from "./ranking-page/ranking-page.component";
-import {SudokuPageComponent} from "./sudoku-page/sudoku-page.component";
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { AuthGuard } from "./auth/auth-guard";
+import { NotFoundPageComponent } from "./not-found-page/not-found-page.component";
+import { RankingPageComponent } from "./ranking-page/ranking-page.component";
+import { SudokuPageComponent } from "./sudoku-page/sudoku-page.component";
 
 export const routes: Routes = [
 
-  {path:'', component:HomePageComponent, canActivate: [AuthGuard] },
-  {path:'login', component:LoginPageComponent},
-  {path:'sudoku',component:SudokuPageComponent},
-  {path:'rankings', component: RankingPageComponent, canActivate: [AuthGuard]},
-  {path:'**', component:NotFoundPageComponent},
+  { path: '', component: HomePageComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'sudoku', component: SudokuPageComponent, canActivate: [AuthGuard] },
+  { path: 'rankings', component: RankingPageComponent, canActivate: [AuthGuard] },
+  { path: '**', component: NotFoundPageComponent },
 
 ];
